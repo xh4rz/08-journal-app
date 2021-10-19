51494fb51dc3a08f96aa34601ce2cbb71c21ae33
Harold Gonzalez <xh4rz@hotmail.com>
2021-10-19 02:16:51 -0500

    7._Estilos_del_AuthRouter

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 export const LoginScreen = () => {
-    return (
-        <div>
-            <h1>LoginScreen</h1>
-        </div>
-    )
-}
+	return (
+		<>
+			<h3>Login</h3>
+			<form>
+				<input type="text" placeholder="Email" name="email" />
+				<input type="password" placeholder="Password" name="password" />
+				<button type="submit">Login</button>
+				<hr />
+				google
+			</form>
+		</>
+	);
+};
