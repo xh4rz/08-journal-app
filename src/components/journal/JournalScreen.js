c547654a90c8beb1a39bb16d858a6d03ec30e9b5
Harold Gonzalez <xh4rz@hotmail.com>
2021-10-31 22:13:19 -0500

    10._Sidebar

--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Sidebar } from './Sidebar';
 
 export const JournalScreen = () => {
-    return (
-        <div>
-            <h1>JournalScreen</h1>
-        </div>
-    )
-}
+	return (
+		<div className="journal__main-content">
+			<Sidebar />
+			<main>
+				<h1>Main Content</h1>
+			</main>
+		</div>
+	);
+};
