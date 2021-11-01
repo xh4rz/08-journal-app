41f9cc12e5832e8935fdd786406ede9791e3ffb9
Harold Gonzalez <xh4rz@hotmail.com>
2021-10-31 21:50:00 -0500

    9._Estilos_en_el_RegisterScreen

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -37,7 +37,9 @@ export const LoginScreen = () => {
 						</p>
 					</div>
 				</div>
-				<Link to="/auth/register">Create new account</Link>
+				<Link to="/auth/register" className="link">
+					Create new account
+				</Link>
 			</form>
 		</>
 	);
