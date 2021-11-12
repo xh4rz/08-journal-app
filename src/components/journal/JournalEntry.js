8b8b47d760e6da8867df0bf1083c6460ca2e6405
Harold Gonzalez <xh4rz@hotmail.com>
2021-11-12 12:56:35 -0500

    11._Sidebar_-_JournalEntries

---
+++ src/components/journal/JournalEntry.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export const JournalEntry = ({ key }) => {
+	console.log(key);
+	return (
+		<div className="journal__entry pointer">
+			<div
+				className="journal__entry-picture"
+				style={{
+					backgroundSize: 'cover',
+					backgroundImage:
+						'url(https://siaguanta.com/wp-content/uploads/2020/05/que-es-wallpaper1.jpg)'
+				}}
+			></div>
+			<div className="journal__entry-body">
+				<p className="journal__entry-title">Un nuevo día</p>
+				<p className="journal__entry-content">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit.
+				</p>
+			</div>
+			<div className="journal__entry-date-box">
+				<span>Monday</span>
+				<h4>28</h4>
+			</div>
+		</div>
+	);
+};
