6b477bd6a2de1b57d8cb8f26a97ea223780c8bbc
Harold Gonzalez <xh4rz@hotmail.com>
2021-12-08 21:39:09 -0500

    11._Manejo_de_errores_del_formulario

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import validator from 'validator';
 import { useForm } from '../hooks/useForm';
 
 export const RegisterScreen = () => {
@@ -14,14 +15,33 @@ export const RegisterScreen = () => {
 
 	const handleRegister = (e) => {
 		e.preventDefault();
+
+		if (isFormValid()) {
+			console.log('Formulario Correcto');
+		}
 	};
 
-	const isFormValid = () => {};
+	const isFormValid = () => {
+		if (name.trim().length === 0) {
+			console.log('Name is required');
+			return false;
+		} else if (!validator.isEmail(email)) {
+			console.log('Email is not valid');
+			return false;
+		} else if (password !== password2 || password.length < 5) {
+			console.log(
+				'Password should be at least 6 characters and match each other'
+			);
+			return false;
+		}
+		return true;
+	};
 
 	return (
 		<>
 			<h3 className="auth__title">Register</h3>
 			<form onSubmit={handleRegister}>
+				<div className="auth__alert-error">Hola Mundo</div>
 				<input
 					type="text"
 					placeholder="Name"
