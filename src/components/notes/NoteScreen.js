7a17441f88b7edb44397005ea2ac5a2caa012be4
Harold Gonzalez <xh4rz@hotmail.com>
2022-01-05 21:47:05 -0500

    11._Activar_una_nota_para_su_edici+¦n

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { useForm } from '../hooks/useForm';
 import { NotesAppBar } from './NotesAppBar';
 
 export const NoteScreen = () => {
+	const { active: note } = useSelector((state) => state.notes);
+
+	const [formValues, handleInputChange] = useForm(note);
+
+	const { body, title } = formValues;
+
+	console.log(formValues);
+
 	return (
 		<div className="notes__main-content">
 			<NotesAppBar />
@@ -11,18 +21,23 @@ export const NoteScreen = () => {
 					placeholder="Some awesme title"
 					className="notes__title-input"
 					autoComplete="off"
+					value={title}
+					onChange={handleInputChange}
 				/>
 				<textarea
 					placeholder="what happened today"
 					className="notes__textarea"
+					value={body}
+					onChange={handleInputChange}
 				></textarea>
-
-				<div className="notes__image">
-					<img
-						src="https://www.discordianos.com/uploads/monthly_2021_05/17010.jpg.21224097e1d74cd8bf7dddbca333a809.jpg"
-						alt="imagen"
-					/>
-				</div>
+				{note.url && (
+					<div className="notes__image">
+						<img
+							src="https://www.discordianos.com/uploads/monthly_2021_05/17010.jpg.21224097e1d74cd8bf7dddbca333a809.jpg"
+							alt="imagen"
+						/>
+					</div>
+				)}
 			</div>
 		</div>
 	);
