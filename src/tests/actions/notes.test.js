9c0e8b7d674dc131fcc03080cd0da578f509080d
Harold Gonzalez <xh4rz@hotmail.com>
2022-01-13 20:08:33 -0500

    8._Pruebas_de_acciones_as+¡ncronas_-_startNewNote

---
+++ src/tests/actions/notes.test.js
@@ -0,0 +1,18 @@
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { startNewNote } from '../../actions/notes';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+const store = mockStore({
+	auth: {
+		uid: 'TESTING'
+	}
+});
+
+describe('Pruebas con las acciones de notes', () => {
+	test('debe de crear una nueva nota startNewNote', async () => {
+		await store.dispatch(startNewNote());
+	});
+});
