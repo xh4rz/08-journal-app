0b768bf3e2ad9a93a91199f829255678540cb928
Harold Gonzalez <xh4rz@hotmail.com>
2022-01-21 22:33:22 -0500

    13._Pruebas_en_las_acciones_de_Auth

---
+++ src/tests/actions/auth.test.js
@@ -0,0 +1,26 @@
+import { login, logout } from '../../actions/auth';
+import { types } from '../../types/types';
+
+describe('Pruebas con las acciones de Auth', () => {
+	test('login y logout deben de crear la acción respectiva', () => {
+		const uid = 'ABC123';
+		const displayName = 'Harold';
+
+		const loginAction = login(uid, displayName);
+		const logoutAction = logout();
+
+		expect(loginAction).toEqual({
+			type: types.login,
+			payload: {
+				uid,
+				displayName
+			}
+		});
+
+		expect(logoutAction).toEqual({
+			type: types.logout
+		});
+
+		// test('debe de realizar el logout', () => {});
+	});
+});
