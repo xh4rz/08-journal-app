8f832a5a9a305ef90ea2b16afb689c4335f7847c
Harold Gonzalez <xh4rz@hotmail.com>
2022-01-30 00:12:03 -0500

    18._Probando_que_el_mensaje_de_error_exista

--- src/tests/components/auth/RegisterScreen.test.js
+++ src/tests/components/auth/RegisterScreen.test.js
@@ -6,10 +6,6 @@ import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import '@testing-library/jest-dom';
 import { RegisterScreen } from '../../../components/auth/RegisterScreen';
-import {
-	startGoogleLogin,
-	startLoginEmailPassword
-} from '../../../actions/auth';
 import { types } from '../../../types/types';
 
 // jest.mock('../../../actions/auth', () => ({
@@ -69,5 +65,30 @@ describe('Pruebas en <RegisterScreen />', () => {
 			type: types.uiSetError,
 			payload: 'Email is not valid'
 		});
+	});
+
+	test('debe de mostrar la caja de alerta con el error', () => {
+		const initState = {
+			auth: {},
+			ui: {
+				loading: false,
+				msgError: 'Email no es correcto'
+			}
+		};
+
+		const store = mockStore(initState);
+
+		const wrapper = mount(
+			<Provider store={store}>
+				<MemoryRouter>
+					<RegisterScreen />
+				</MemoryRouter>
+			</Provider>
+		);
+
+		expect(wrapper.find('.auth__alert-error').exists()).toBe(true);
+		expect(wrapper.find('.auth__alert-error').text().trim()).toBe(
+			initState.ui.msgError
+		);
 	});
 });
