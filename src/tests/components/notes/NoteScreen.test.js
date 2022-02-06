3b04f05c55b8f50c99984ec26e1d95f38f4b7153
Harold Gonzalez <xh4rz@hotmail.com>
2022-02-06 18:23:23 -0500

    21._Pruebas_en_el_NoteScreen

---
+++ src/tests/components/notes/NoteScreen.test.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import '@testing-library/jest-dom';
+import { activeNote } from '../../../actions/notes';
+import { NoteScreen } from '../../../components/notes/NoteScreen';
+
+jest.mock('../../../actions/notes', () => ({
+	activeNote: jest.fn()
+}));
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+const initState = {
+	auth: {
+		uid: '1',
+		name: 'Harold'
+	},
+	ui: {
+		loading: false,
+		msgError: null
+	},
+	notes: {
+		active: {
+			id: 1234,
+			title: 'hola',
+			body: 'mundo',
+			date: 0
+		},
+		notes: []
+	}
+};
+
+let store = mockStore(initState);
+store.dispatch = jest.fn();
+
+const wrapper = mount(
+	<Provider store={store}>
+		<NoteScreen />
+	</Provider>
+);
+
+describe('Pruebas en <NoteScreen />', () => {
+	test('debe de mostrarse correctamente', () => {
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	test('debe de disparar el active note', () => {
+		wrapper.find('input[name="title"]').simulate('change', {
+			target: {
+				name: 'title',
+				value: 'Hola de nuevo'
+			}
+		});
+	});
+
+	expect(activeNote).toHaveBeenLastCalledWith(1234, {
+		body: 'mundo',
+		title: 'hola',
+		id: 1234,
+		date: 0
+	});
+});
